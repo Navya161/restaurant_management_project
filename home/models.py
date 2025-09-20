0f3a2775c29ce8aa779a76d5fc17693d77941d08
Navya161 <navya16122005@gmail.com>
2025-09-20 21:37:21 +0530

    49913

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class MenuCategory(models.Models):
+    name=models.CharField(max_length=100)
+    def__str__(self)
+      return  self.name
 
 # Create your models here.
