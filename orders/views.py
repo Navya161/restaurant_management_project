a77c20589eb5405def115d6b296710659acb561a
Navya161 <navya16122005@gmail.com>
2025-09-24 21:01:26 +0530

    49912

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from.utils import generate_unique_coupon_code
+from.models import Coupon
+def Create_coupon():
+    code =generate_unique_coupon_code(Coupon,field="code",length=12)
+    coupon = Coupon.objects.create(code=code,discount=10)
+    return coupon
+    
