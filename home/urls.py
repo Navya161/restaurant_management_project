de8f0661070358eb388d9dbc40cd2d3411f615a6
Navya161 <navya16122005@gmail.com>
2025-11-03 17:05:16 +0530

    58282

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,9 @@ from .views import FeaturedMenuItemsView
 
 urlpatterns = [
     path('menu/featured/', FeaturedMenuItemsView.as_view(), name='featured-menu-items'),
+]from django.urls import path
+from .views import OrderHistoryView
+
+urlpatterns = [
+    path('orders/history/', OrderHistoryView.as_view(), name='order-history'),
 ]
