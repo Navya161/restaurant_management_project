55cc1b74d35d16863650302e8c6b04b8ae647ac4
Navya161 <navya16122005@gmail.com>
2025-11-03 17:31:13 +0530

    58280

---
+++ restaurant_management/__init__.py
@@ -0,0 +1,11 @@
+
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    has_delivery = models.BooleanField(default=False)  # ✅ new field
+
+    def __str__(self):
+        return self.name
