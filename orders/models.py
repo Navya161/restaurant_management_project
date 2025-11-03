caf244ce7142f2d83589cc0502fd9481f0c18071
Navya161 <navya16122005@gmail.com>
2025-11-03 16:51:04 +0530

    49910

--- orders/models.py
+++ orders/models.py
@@ -12,4 +12,18 @@ class OrderStatus(models.Model):
     CANCELLED = 'Cancelled'
 
     def _str_(self):
-        return self.name
+        return selfrom django.db import models
+from .models import OrderStatus  # make sure this import exists or adjust if both models are in same file
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=20)
+    customer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    # 👇 Add this line
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def _str_(self):
+        return self.order_number
+4:50 PM
+f.name
