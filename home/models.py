ff629b007feb5219cf083edd151ac07dccdfd667
Navya161 <navya16122005@gmail.com>
2025-11-03 17:00:00 +0530

    58283

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 class MenuCategory(models.Models):
     name=models.CharField(max_length=100)
-    def__str__(self)
-      return  self.name
-
+    
 # Create your models here.
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)  # 👈 NEW FIELD
+
+    def __str__(self):
+        return self.name
