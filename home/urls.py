0c861ca7bf763c4762f3c53f9ad24152acd6dc04
Navya161 <navya16122005@gmail.com>
2025-11-03 17:01:05 +0530

    58283

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     
+]from django.urls import path
+from .views import FeaturedMenuItemsView
+
+urlpatterns = [
+    path('menu/featured/', FeaturedMenuItemsView.as_view(), name='featured-menu-items'),
 ]
