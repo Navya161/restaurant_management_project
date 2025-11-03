64bfbe24b1cf1afa19c794cb50a525dba8a9419e
Navya161 <navya16122005@gmail.com>
2025-11-03 16:48:43 +0530

    49911

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    PENDING = 'Pending'
+    PROCESSING = 'Processing'
+    COMPLETED = 'Completed'
+    CANCELLED = 'Cancelled'
+
+    def _str_(self):
+        return self.name
